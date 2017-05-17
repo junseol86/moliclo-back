3673431dee138cc7cae11bd087bff5fc32a1370c
hyeonmin <junseol86@gmail.com>
2017-05-18 02:26:32 +0900

    Add user thumbnail column

--- src/main/scala/com/moliclo/http/HttpService.scala
+++ src/main/scala/com/moliclo/http/HttpService.scala
@@ -1,7 +1,7 @@
 package com.moliclo.http
 
 import akka.http.scaladsl.server.Directives._
-import com.moliclo.http.routes.PostingRoute
+import com.moliclo.http.routes.v1.PostingRoute
 import com.moliclo.utils.CorsSupport
 
 trait HttpService extends CorsSupport with PostingRoute {
